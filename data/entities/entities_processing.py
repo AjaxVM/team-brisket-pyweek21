a1281ab00cde1c5960f9abf0d2257b4dac720d05
Colin Hartwig <hartwig3@gmail.com>
2016-02-29 21:58:10 -0600

    Update entity processing to be dynamic
    This will allow us to add any properties that we need to our
    config, which we can then use throughout the application. By
    using this setup, the processing is easier, and tiles,
    characters, platforms and whatever other entities we may have
    can have the properties that they need.

--- data/entities/entities_processing.py
+++ data/entities/entities_processing.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, json, print_function, unicode_literals
 
 
 def process_json(json_to_parse):
@@ -6,23 +6,24 @@ def process_json(json_to_parse):
     Takes json from the server or the client and xforms it into a logical entity and a graphical entity
     '''
 
-    logical_entity = LogicalEntity(json_to_parse.get('hit_box_size'), json_to_parse.get('state'))
-    graphical_entity = GraphicalEntity(json_to_parse.get('resource'))
+    # Allows for any number of properties to be defined within the configs
+    # which will be pulled into the entities' constructors.
+    logical_entity = LogicalEntity(json_to_parse.get('logicalConfig'))
+    graphical_entity = GraphicalEntity(json_to_parse.get('graphicalConfig'))
 
     return [logical_entity, graphical_entity]
 
 
 class LogicalEntity(object):
 
-    def __init__(self, hit_box_size, state):
-        # length and width of hit box
-        self.hit_box_size = hit_box_size
-        self.state = state
+    def __init__(self, config):
+        # Take the config and set the object to what is in the config
+        self.__dict__.update(config)
 
 
 class GraphicalEntity(object):
 
-    def __init__(self, resource):
-        # Was thinking resource should just be a string of a file location for an image? We can change later
-        self.resource = resource
+    def __init__(self, config):
+        # Take the config and set the object to what is in the config
+         self.__dict__.update(config)
 
