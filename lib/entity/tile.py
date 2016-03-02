d98d5ba1dc77c3ee6f319645ac46761749d35d26
Charlie Ciccia <cciccia@healthgrades.com>
2016-03-02 16:56:29 -0600

    adding collision detection with behavior mod adding tile

--- lib/entity/tile.py
+++ lib/entity/tile.py
@@ -1,32 +1,24 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from .base import Entity as BaseEntity, Vec, Bbox, move_bbox
+from .base import Entity as BaseEntity, Bbox
 
 class Entity(BaseEntity):
 
-    hWidth = 5.0
-    height = 10.0
+    hWidth = 20.0
+    height = 20.0
 
     def __init__(self, x, y, vx, vy):
         super(Entity, self).__init__()
         self.bbox = Bbox(x, y, self.hWidth, self.height)
-        self.velocity = Vec(vx, vy)
 
     def get_next_state(self):
-        return dict(
-            bbox=move_bbox(self.bbox, self.velocity)
-        )
+        return dict()
 
     def get_fail_state(self):
-        return dict(
-            velocity=Vec(-self.velocity.x, -self.velocity.y)
-        )
+        return dict()
 
     def state_repr(self):
         return dict(
             x=self.bbox.x,
             y=self.bbox.y,
-            vx=self.velocity.x,
-            vy=self.velocity.y
         )
-    
--- lib/server/game.py
+++ lib/server/game.py
@@ -5,7 +5,8 @@ import json
 from .level_loader import LevelLoader
 from twisted.application import service
 from twisted.internet import reactor
-from ..entity.ball import Ball
+from ..entity import ball
+from ..entity.base import bbox_collides
 
 
 log = logging.getLogger(__name__)
@@ -19,7 +20,7 @@ class GameServer(service.Service):
         self.level = LevelLoader('level1')
         
         self.entities = [
-            Ball(
+            ball.Entity(
                 random.uniform(0, 640),
                 random.uniform(0, 480),
                 random.uniform(-5, 5),
@@ -47,13 +48,25 @@ class GameServer(service.Service):
 
     def tick(self):
 
-        entity_states = {}
+        failures = {}
+
+        potential_entity_states = {hash(entity): entity.get_next_state() for entity in self.entities}
+
+        for key_a, entity_a in potential_entity_states.iteritems():
+            for key_b, entity_b in potential_entity_states.iteritems():
+                if key_a < key_b:
+                    if bbox_collides(entity_a['bbox'], entity_b['bbox']):
+                        log.debug('collision detected')
+                        failures[key_b] = 1
 
         for entity in self.entities:
-            entity.tick()
-            entity_states[hash(entity)] = entity.state_repr()
-            
-        self.router.broadcast(entity_states)
+            key = hash(entity)
+            if key in failures:
+                entity.set_next_state(**entity.get_fail_state())
+            else:
+                entity.set_next_state(**potential_entity_states[key])
+                                                            
+        self.router.broadcast({hash(entity): entity.state_repr() for entity in self.entities})
 
 
         if self.is_running:
