29091f7187665bf8a301e5634179e10dd40b0c15
Cameron Bjorklund <cbjorklund@healthgrades.com>
2016-03-03 13:02:03 -0600

    Made entities with less arguments

--- lib/entity/entities.py
+++ lib/entity/entities.py
@@ -1,45 +1,37 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from .. import settings
 from .base import Entity as BaseEntity, Bbox, Vec
 
 class ZombieEntity(BaseEntity):
 
-    def __init__(
-        self,
+    def __init__(self):
         alive=True,
         bbox=Bbox(0, 0, 0, 0),
         velocity=Vec(0, 0),
-        state='idle',
-        resource='path/to/image.png',
+        resource = os.path.join(settings.DATA_DIR, 'assets', 'entities', 'zombie.png'),
         is_enviornment=False
-    ):
         super(ZombieEntity, self).__init__(alive, bbox, velocity, state, resource, is_enviornment)
 
 
 class PlayerEntity(BaseEntity):
 
-    def __init__(
-        self,
+    def __init__(self):
         alive=True,
         bbox=Bbox(0, 0, 0, 0),
         velocity=Vec(0, 0),
-        state='idle',
-        resource='path/to/image.png',
+        resource = os.path.join(settings.DATA_DIR, 'assets', 'entities', 'testplayer.png'),
         is_enviornment=False
-    ):
         super(PlayerEntity, self).__init__(alive, bbox, velocity, state, resource, is_enviornment)
 
 
 class WallEntity(BaseEntity):
 
-    def __init__(
-        self,
-        alive=True,
+    def __init__(self):
+        alive=False,
         bbox=Bbox(0, 0, 0, 0),
         velocity=Vec(0, 0),
-        state='idle',
         resource='path/to/image.png',
         is_enviornment=True
-    ):
         super(WallEntity, self).__init__(alive, bbox, velocity, state, resource, is_enviornment)
 
